7ca75633fdeeafe66772a28dd5221634a0b2fd1e
JonathanTippy <jonathan@tippy.name>
2025-06-09 12:47:19 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,234 @@
+mod util;
+
+extern crate discord;
+
+use discord::model::{Event, Reaction, ReactionEmoji, ChannelId, ServerId, MessageId};
+use discord::Discord;
+use discord::Connection;
+use std::env;
+use std::time::Instant;
+use std::vec;
+use util::{get_rating, cull_meme_cache, reproduce_message};
+
+use std::collections::HashMap;
+
+use simple_user_input::get_input;
+
+mod simple_user_input {
+    use std::io;
+    pub fn get_input(prompt: &str) -> String{
+        println!("{}",prompt);
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        input.trim().to_string()
+    }
+}
+
+const BOT_CACHE_NAME:&str = "botcache";
+
+const MEME_CACHE_DEPTH:usize = 20; // more than about thirty might hit the character limit
+
+struct ServerMap {
+    memes_channel_id: u64
+    , funny_memes_channel_id: u64
+}
+
+fn main() {
+
+
+    let mut channel_to_server_map = HashMap::new();
+    let mut funny_channels_map = HashMap::new();
+    let mut original_to_reproduction_map = HashMap::new();
+
+    // Log in to Discord using a bot token from the environment
+    let start = Instant::now();
+    let discord = Discord::from_bot_token(&env::var("DISCORD_TOKEN").expect("Expected token"))
+        .expect("login failed");
+
+    // Establish and use a websocket connection
+    let (mut connection, ready_event) = discord.connect().expect("connect failed");
+    let duration = start.elapsed();
+
+    let servers = discord.get_servers().expect("Failed to fetch servers");
+    let mut botcache_id = MessageId(0);
+    let mut botcache_channel_id = ChannelId(0);
+    for server in servers {
+        let channels = discord.get_server_channels(server.id).unwrap();
+        let mut found:bool = false;
+        for channel in channels {
+            channel_to_server_map.insert(channel.id, server.id);
+            if channel.name == BOT_CACHE_NAME {
+
+                match channel.last_message_id {
+                    Some(message_id) => {
+                        match discord.get_message(channel.id, message_id) {
+                            Ok(message) => {let map = message.content.split("\n").collect::<Vec<_>>()[0];
+                                funny_channels_map.insert(
+                                    ChannelId(map.split(":").collect::<Vec<_>>()[0].parse().unwrap())
+                                    , ChannelId(map.split(":").collect::<Vec<_>>()[1].parse().unwrap())
+                                );
+                                for line in message.content.split("\n") {
+                                    if line==map {continue}
+                                    original_to_reproduction_map.insert(
+                                        MessageId(line.split(":").collect::<Vec<_>>()[0].parse().unwrap())
+                                        , MessageId(line.split(":").collect::<Vec<_>>()[1].parse().unwrap())
+                                    );
+                                }
+                                botcache_id=message.id;
+                                botcache_channel_id=channel.id;
+                            }
+
+                            Err(message) => {let map: String = get_input("please provide the 'memes' to 'funny memes' map in the form of ID:ID");
+                                let sent = discord.send_message(channel.id, &map, "", false).unwrap();
+                                funny_channels_map.insert(
+                                    ChannelId(map.split(":").collect::<Vec<_>>()[0].parse().unwrap())
+                                    , ChannelId(map.split(":").collect::<Vec<_>>()[1].parse().unwrap())
+                                );
+                                for line in sent.content.split("\n") {
+                                    if line==map {continue}
+                                    original_to_reproduction_map.insert(
+                                        MessageId(line.split(":").collect::<Vec<_>>()[0].parse().unwrap())
+                                        , MessageId(line.split(":").collect::<Vec<_>>()[1].parse().unwrap())
+                                    );
+                                }
+                                botcache_id=sent.id;
+                                botcache_channel_id=channel.id;
+                            }
+                        }
+
+                    }
+                    None => {
+                        let map: String = get_input("please provide the 'memes' to 'funny memes' map in the form of ID:ID");
+                        let sent = discord.send_message(channel.id, &map, "", false).unwrap();
+                        funny_channels_map.insert(
+                            ChannelId(map.split(":").collect::<Vec<_>>()[0].parse().unwrap())
+                            , ChannelId(map.split(":").collect::<Vec<_>>()[1].parse().unwrap())
+                        );
+                        for line in sent.content.split("\n") {
+                            if line==map {continue}
+                            original_to_reproduction_map.insert(
+                                MessageId(line.split(":").collect::<Vec<_>>()[0].parse().unwrap())
+                                , MessageId(line.split(":").collect::<Vec<_>>()[1].parse().unwrap())
+                            );
+                        }
+                        botcache_id=sent.id;
+                        botcache_channel_id=channel.id;
+                    }
+                }
+                found = true;
+            }
+        }
+    }
+
+
+    println!("Time elapsed connecting: {:?}", duration);
+    println!("Ready.");
+    loop {
+        match connection.recv_event() {
+            Ok(Event::MessageCreate(message)) => {
+                println!("{} says: {}", message.author.name, message.content);
+                if message.content == "!test" {
+                    let start = Instant::now();
+                    let _ = discord.send_message(
+                        message.channel_id,
+                        "This is a reply to the test.",
+                        "",
+                        false,
+                    );
+                    let duration = start.elapsed();
+                    println!("Time elapsed sending a message: {:?}", duration);
+                } else if message.content == "!quit" {
+                    println!("Quitting.");
+                    break;
+                }
+            }
+            // funny bot action
+            Ok(Event::ReactionAdd(reaction)) | Ok(Event::ReactionRemove(reaction)) => {
+
+                    match Discord::get_message(&discord, reaction.channel_id, reaction.message_id) {
+                        Ok(message) => {
+                            if (funny_channels_map.contains_key(&message.channel_id)) {
+
+                                let rating = get_rating(&discord, &message);
+                                println!("rating: {}", rating);
+
+                                if rating > 7.999 {
+
+
+                                    let message_reproduction = reproduce_message(message.clone(), rating, &channel_to_server_map);
+                                    if !original_to_reproduction_map.contains_key(&message.id) {
+                                        let sent = discord.send_message(
+                                            *funny_channels_map.get(&message.channel_id).unwrap()
+                                            , &message_reproduction
+                                            , ""
+                                            , false
+                                        ).unwrap();
+                                        original_to_reproduction_map.insert(message.id, sent.id);
+                                        let _ = discord.edit_message(
+                                            botcache_channel_id
+                                            , botcache_id
+                                            , &format!(
+                                                "{}\n{}:{}"
+                                                , discord.get_message(botcache_channel_id, botcache_id).unwrap().content
+                                                , message.id
+                                                , sent.id
+                                            )
+                                        );
+                                    } else {
+                                        let message_reproduction = reproduce_message(message.clone(), rating, &channel_to_server_map);
+
+                                        let _ = discord.edit_message(
+                                            *funny_channels_map.get(&message.channel_id).unwrap()
+                                            , *original_to_reproduction_map.get(&message.id).unwrap()
+                                            , &message_reproduction
+                                        );
+                                    }
+                                } else {
+                                    if original_to_reproduction_map.contains_key(&message.id) {
+                                        let _ = discord.delete_message(*funny_channels_map.get(&message.channel_id).unwrap(), *original_to_reproduction_map.get(&message.id).unwrap());
+                                        let botcache = discord.get_message(botcache_channel_id, botcache_id).unwrap();
+                                        let mut old_botcache = botcache.content.split("\n").collect::<Vec<_>>();
+                                        old_botcache.pop();
+                                        let _ = discord.edit_message(
+                                            botcache_channel_id
+                                            , botcache_id
+                                            , &old_botcache.join("\n")
+                                        );
+                                        original_to_reproduction_map.remove(&message.id);
+                                    }
+                                }
+
+                            }
+
+
+                        }
+                        Err(message) => {println!("couldn't get the message")}
+                    }
+
+
+                //let reaction_emoji = reaction.emoji;
+
+                let reaction_emoji;
+
+                match reaction.emoji {
+                    ReactionEmoji::Unicode(string) => reaction_emoji=string,
+                    ReactionEmoji::Custom { name: name, id: _ } => reaction_emoji=name,
+                }
+
+                println!("{} added or removed {} on {}", reaction.user_id, reaction_emoji, reaction.message_id);
+
+                let botcache = discord.get_message(botcache_channel_id, botcache_id).unwrap();
+                cull_meme_cache(&discord, &botcache, MEME_CACHE_DEPTH);
+            }
+            Ok(_) => {}
+            Err(discord::Error::Closed(code, body)) => {
+                println!("Gateway closed on us with code {:?}: {}", code, body);
+                break;
+            }
+            Err(err) => println!("Receive error: {:?}", err),
+        }
+    }
+}
---
+++ src/util.rs
@@ -0,0 +1,108 @@
+extern crate discord;
+
+use discord::model::{Event, Reaction, ReactionEmoji, ChannelId, ServerId,Message};
+use discord::Discord;
+use discord::Connection;
+use std::env;
+use std::time::Instant;
+use std::vec;
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+
+
+pub fn get_rating(discord:&Discord, message:&Message) -> f32 {
+
+    let mut emoji_value_map = HashMap::new();
+    emoji_value_map.insert("0️⃣", 0 );
+    emoji_value_map.insert("1️⃣", 1 );
+    emoji_value_map.insert("2️⃣", 2 );
+    emoji_value_map.insert("3️⃣", 3 );
+    emoji_value_map.insert("4️⃣", 4 );
+    emoji_value_map.insert("5️⃣", 5 );
+    emoji_value_map.insert("6️⃣", 6 );
+    emoji_value_map.insert("7️⃣", 7 );
+    emoji_value_map.insert("8️⃣", 8 );
+    emoji_value_map.insert("9️⃣", 9 );
+    emoji_value_map.insert("🔟", 10 );
+    let mut users_who_rated = HashSet::new();
+    let mut user_ratings_map = HashMap::new();
+
+    //let mut ratings = Vec::new();
+
+    for r in message.reactions.clone() {
+
+        //let mut all_emoji:String = String::new();
+        let reaction_emoji:String;
+        match r.emoji.clone() {
+            ReactionEmoji::Unicode(string) => reaction_emoji=string,
+            ReactionEmoji::Custom { name: name, id: _ } => reaction_emoji=name,
+        }
+        if emoji_value_map.contains_key(reaction_emoji.as_str()) {
+            let rating= *emoji_value_map.get(reaction_emoji.as_str()).unwrap();
+            let mut users = discord
+                .get_reactions(
+                    message.channel_id,
+                    message.id,
+                    r.emoji,
+                    Some(50), // Max users per request (adjust as needed, max 100)
+                    None, // No 'after' for pagination initially
+                ).unwrap();
+            for u in &users {
+                user_ratings_map.insert(u.id, rating);
+                users_who_rated.insert(u.id);
+            }
+        }
+    }
+    let mut ratings = Vec::new();
+    for u in users_who_rated {
+        ratings.push(*user_ratings_map.get(&u).unwrap());
+    }
+    let mut rating = 0.0;
+    if (ratings.len()>1) {
+        rating = ratings.iter().sum::<i32>() as f32 / ratings.len() as f32;
+        /*if ratings.len() < 3 {
+            rating = ratings.iter().sum::<i32>() as f32 / ratings.len() as f32;
+        } else {
+            ratings.sort();
+            rating = (ratings.iter().sum::<i32>() - *ratings.first().unwrap()) as f32 / (ratings.len()-1) as f32;
+        }*/
+    }
+    return rating;
+}
+
+pub fn cull_meme_cache(discord:&Discord, meme_cache:&Message, max_len:usize, ) {
+    if &meme_cache.content.split("\n").collect::<Vec<_>>().len()>&max_len {
+
+        let extras = &meme_cache.content.split("\n").collect::<Vec<_>>().len()-(max_len);
+
+        let old_cache = &meme_cache.content;
+        let mut new_cache = String::new();
+        let i = 0;
+        for line in old_cache.split("\n").collect::<Vec<_>>() {
+            if i==0 {
+                new_cache = new_cache + line; continue
+            }
+            if i>extras {
+                new_cache = new_cache + line;
+            }
+        }
+        let _ = discord.edit_message(meme_cache.channel_id, meme_cache.id, &new_cache);
+    }
+}
+
+pub fn reproduce_message(original:Message, rating:f32, channel_to_server_map:&HashMap<ChannelId, ServerId>) -> String {
+
+    let mut all_attachment_urls:String = String::new();
+    for att in original.attachments {
+        all_attachment_urls = format!("{} {}", all_attachment_urls, att.url);
+    }
+
+    let og_message_link = format!("https://discord.com/channels/{}/{}/{}", channel_to_server_map.get(&original.channel_id).unwrap(), original.channel_id, original.id);
+
+    if original.content.trim() == "" {
+        return format!("Posted by: {}\nLink: {}\nAvg rating: {}\n————————\n{}", original.author.name, og_message_link, rating, &all_attachment_urls)
+    }
+    format!("Posted by: {}\nLink: {}\nAvg rating: {}\n————————\n{}\n{}", original.author.name, og_message_link, rating, original.content, &all_attachment_urls)
+}
